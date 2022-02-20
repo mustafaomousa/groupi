2dcd8d321f9e23ebbc831b4e657ef55feebf6997
mustafaomousa <mustafaomousa@icloud.com>
2022-02-20 10:34:25 -0600

    updated the user model

--- app/models/user.py
+++ app/models/user.py
@@ -5,10 +5,10 @@ from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
-    email = db.Column(db.String, nullable=False, unique=True)
-    f_name = db.Column(db.String, nullable=False)
-    l_name = db.Column(db.String, nullable=False)
+    username = db.Column(db.String(64), nullable=False, unique=True)
+    email = db.Column(db.String(264), nullable=False, unique=True)
+    f_name = db.Column(db.String(50), nullable=False)
+    l_name = db.Column(db.String(50), nullable=False)
     dob = db.Column(db.Date, nullable=False)
     profile_picture = db.Column(db.Text)
     bio = db.Column(db.Text)
