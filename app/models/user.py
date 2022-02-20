845e56bcbfbb5585dcd9ed4c8aeb8799e1ece506
mustafaomousa <mustafaomousa@icloud.com>
2022-02-20 10:13:23 -0600

    created user model

---
+++ app/models/user.py
@@ -0,0 +1,38 @@
+import imp
+from .db import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(40), nullable=False, unique=True)
+    email = db.Column(db.String, nullable=False, unique=True)
+    f_name = db.Column(db.String, nullable=False)
+    l_name = db.Column(db.String, nullable=False)
+    dob = db.Column(db.Date, nullable=False)
+    profile_picture = db.Column(db.Text)
+    bio = db.Column(db.Text)
+    hashed_password = db.Column(db.String(255), nullable=False)
+
+    @property
+    def password(self):
+        return self.hashed_password
+    
+    @password.setter
+    def password(self, password):
+        self.hashed_password = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+            'f_name': self.f_name,
+            'l_name': self.l_name,
+            'dob': self.dob,
+            'profile_picture': self.profile_picture,
+            'bio': self.bio,
+        }
