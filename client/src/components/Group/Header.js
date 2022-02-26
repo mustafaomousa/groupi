987ebc23559cf67130502cf7d5700e0d5a8dd5ba
Mustafa <mustafa@Abrahim.local>
2022-02-25 20:06:38 -0600

    made group profile responsive

--- client/src/components/Group/Header.js
+++ client/src/components/Group/Header.js
@@ -2,13 +2,17 @@ const Header = ({ group }) => {
   return (
     <div className="flex w-full p-2 text-3xl">
       <div className="flex items-center space-x-2 text-zinc-900">
-        <img
-          src={group.profile_picture}
-          className="h-16 w-16 rounded bg-white shadow"
-        />
-        <div className="flex h-full flex-col justify-end text-sm">
+        {group.profile_picture ? (
+          <img
+            src={group.profile_picture}
+            className="h-12 w-12 rounded bg-white md:h-14 md:w-14"
+          />
+        ) : (
+          <div className="h-12 w-12 rounded bg-white md:h-14 md:w-14" />
+        )}
+        <div className="flex h-full flex-col justify-end text-xs md:text-sm">
           <p className="font-bold">{group.name}</p>
-          <p className="">{group.bio}</p>
+          <p className="antialiased">{group.bio}</p>
         </div>
       </div>
     </div>
